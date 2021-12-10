16dc9fa5884ace427346cb764660fa72706b8ea0
StefanniBianca <91340404+StefanniBianca@users.noreply.github.com>
2021-12-10 16:24:45 -0300

    Add files via upload

---
+++ serasi.js
@@ -0,0 +1,10 @@
+﻿let show = true;
+
+const menu = document.querySelector(".menu")
+const menu2 = menu.querySelector(".menu2")
+
+menu2.addEventListener("click", () => {
+    document.body.style.overflow = show ? "hidden" : "initial"
+    menu.classList.toggle("on", show)
+    show = !show;
+})
